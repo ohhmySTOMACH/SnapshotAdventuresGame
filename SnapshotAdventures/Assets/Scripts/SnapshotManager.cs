f3cbe3932b7f4b7bdb1ca62c420902ae8672b590
ohhmySTOMACH <49881990+ohhmySTOMACH@users.noreply.github.com>
2023-09-04 19:54:34 +1000

    Update draft map scene and camera UI

--- SnapshotAdventures/Assets/Scripts/SnapshotManager.cs
+++ SnapshotAdventures/Assets/Scripts/SnapshotManager.cs
@@ -2,27 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnapshotManager : MonoBehaviour
+namespace Camera
 {
-    public List<Texture2D> cameraRoll = new List<Texture2D>();
-    // Start is called before the first frame update
-    void Start()
+    public class SnapshotManager : MonoBehaviour
     {
-        
-    }
+        public List<Texture2D> cameraRoll = new List<Texture2D>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) {
-            CapturePhotos();
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0)) {
+                CapturePhotos();
+            }
+        }
+
+        void CapturePhotos() 
+        {
+            Texture2D capturedScreenshot = ScreenCapture.CaptureScreenshotAsTexture();
+            cameraRoll.Add(capturedScreenshot);
+            Debug.Log("Photo added to camera roll.");
         }
     }
-
-    void CapturePhotos() 
-    {
-        Texture2D capturedScreenshot = ScreenCapture.CaptureScreenshotAsTexture();
-        cameraRoll.Add(capturedScreenshot);
-        Debug.Log("Photo added to camera roll.");
-    }
 }
+
